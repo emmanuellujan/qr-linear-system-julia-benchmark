d9ebf43d1ab2a3cd328a03c55c775fb41ddc7d69
Emmanuel Lujan <lujan.emmanuel@gmail.com>
2021-10-22 16:38:03 -0400

    Improvements to MGS performance.

--- qrtest.jl
+++ qrtest.jl
@@ -2,7 +2,7 @@ using LinearAlgebra
 using Octavian
 using .Threads
 
-# Julia adaptation of the following matlab code: https://rpubs.com/Barry_Daemi/682958
+# Julia adaptation of the following Matlab code: https://rpubs.com/Barry_Daemi/682958
 
 # Usage: [Q,R] = MGS(A)
 #
@@ -16,32 +16,33 @@ using .Threads
 # Outputs: Q      The orthogonal matrix: Q
 #          R      The upper triangular matrix: R
 
-@views function MGS(A::Matrix{Float64})
-    ~, n = size(A)
-    Q = copy(A)
-    R = zeros(n,n)
-    @threads for j = 1:n
+
+function MGS2( m::Int64, n::Int64, nn::Float64,
+                    Q::Matrix{Float64}, R::Matrix{Float64})
+    @views for j = 1:n
         nn = norm(Q[:,j])
         R[j,j] = nn
         Q[:,j] /= nn
-        
-        aux = matmul(Q[:,j]', Q[:,j+1:n])
-        
-        R[j,j+1:n] = aux
-#        matmul!(reshape(R[j,j+1:n], 1, :), Q[:,j]', Q[:,j+1:n])
-
-         Q[:,j+1:n] -= matmul(hcat(Q[:,j]), aux)
-#        Q[:,j+1:n] -= hcat(Q[:,j]) * R[j,j+1:n]'
-#        Q[:,j+1:n] -= matmul( reshape(Q[:,j], :, 1), reshape(R[j,j+1:n], 1, :))
-#        Q[:,j+1:n] -= matmul(hcat(Q[:,j]), hcat(R[j,j+1:n]'))
-#        Q[:,j+1:n] -= matmul( Q[:,j][:,:], R[j,j+1:n][:,:]')
-
+        aux1 = reshape(Q[:,j], :, 1)
+        aux2 = matmul(Q[:,j]', Q[:,j+1:n])
+        R[j,j+1:n] = aux2
+        Q[:,j+1:n] -= matmul(aux1, aux2)
     end
+end
+
+function MGS(A::Matrix{Float64})
+    m, n = size(A)
+    nn = 0.0
+    Q = copy(A)
+    R = zeros(Float64, n, n)
+    MGS2(m, n, nn, Q, R)
     return Q, R
 end
 
-A = rand(50, 50)
-B = rand(50, 1)
+m = 500
+n = 30
+A = rand(m, n)
+B = rand(m, 1)
 
 @time begin
 X1 = A \ B
@@ -52,18 +53,11 @@ X2 = qr(A, Val(true)) \ B
 end
 
 @time begin
-Q3, R3 = qr(A)
-X3 = R3 \ (transpose(Q3) * B)
+Q3, R3 = MGS(A)
+X3 = R3 \ matmul(transpose(Q3), B)
 end
-
-@time begin
-Q4, R4 = MGS(A)
-X4 = R4 \ matmul(transpose(Q4), B)
-end
+;
 
 println(maximum(X1 - X2))
 println(maximum(X1 - X3))
-println(maximum(X1 - X4))
-
-
 
